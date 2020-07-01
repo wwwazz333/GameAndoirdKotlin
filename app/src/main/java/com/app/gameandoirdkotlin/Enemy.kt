bb9fb934405a952061aa75c2e9f9f16f29e3b01f
wwwazz333 <wwwazz333@gmail.com>
2020-07-01 23:57:56 +0200

    abstact class (before) améliorer IA

--- app/src/main/java/com/app/gameandoirdkotlin/Enemy.kt
+++ app/src/main/java/com/app/gameandoirdkotlin/Enemy.kt
@@ -4,23 +4,13 @@ import android.graphics.*
 
 
 
-class Enemy(surface: Drawing, x:Int, y:Int) {
+class Enemy(surface: Drawing, x:Int, y:Int):Entity(surface, x, y) {
 
-
-
-    var x:Int = x
-    var y:Int = y
-    val rect:Rect
-    var canJump = true
-    val imageToRight:Bitmap
-    val imageToLeft:Bitmap
-    var surface:Drawing? = null
-    var isJumping = false
-    var cntJump = 0
-    var direction = 0
+    override val rect:Rect
+    override val imageToRight:Bitmap
+    override val imageToLeft:Bitmap
 
     init {
-        this.surface = surface
         var bitmapTemp = BitmapFactory.decodeResource(surface.resources, R.drawable.enemy1_right)
         imageToRight = Bitmap.createScaledBitmap(bitmapTemp, (bitmapTemp.width*0.2).toInt(),
             (bitmapTemp.height*0.2).toInt(),true)
@@ -32,18 +22,7 @@ class Enemy(surface: Drawing, x:Int, y:Int) {
 
     }
 
-    fun update(){
-        rect.set(this.x,this.y,this.x+imageToRight.width,this.y+imageToRight.height)
-
-    }
-    fun draw(canvas: Canvas?){
-        update()
-
-        if(direction == 1)canvas!!.drawBitmap(imageToRight,rect.left.toFloat(),rect.top.toFloat(), Paint())
-        else if(direction == 0)canvas!!.drawBitmap(imageToLeft,rect.left.toFloat(),rect.top.toFloat(), Paint())
-    }
-
-    fun actions(player:Player, list: List<Rectangle>){
+    override fun actions(player:Player, list: List<Rectangle>){
         if(player.rect.centerX() < rect.centerX()) {
             direction = 0
             move(-speedEnemy, list)
@@ -64,53 +43,8 @@ class Enemy(surface: Drawing, x:Int, y:Int) {
         gravity(list)
     }
 
-    fun move(nbr:Int, list:List<Rectangle>){
-        if(!touchOne(list)){
-            x+=nbr
-            update()
-            if(touchOne(list)) x-=nbr
-        }
-        else{
-            x+=nbr
-            update()
-            if(touchOne(list)) x-=nbr
-        }
-        update()
+    override fun actions(toRight: Boolean, toLeft: Boolean, list: List<Rectangle>) {
+        TODO("résoudre pb des  overrides")
     }
 
-    fun touch(rect: Rectangle):Boolean{
-        return this.rect.right >= rect.x && this.rect.left <= rect.x+rect.w && this.rect.top <= rect.y+rect.h && this.rect.bottom >= rect.y
-    }
-    fun touch(p: Player):Boolean{
-        return rect.right >= p.x && rect.left <= p.rect.right && rect.top <= p.rect.bottom && rect.bottom >= p.y
-    }
-
-    fun touchOne(list:List<Rectangle>):Boolean{
-        for(rect in list){
-            if(touch(rect)) return true
-        }
-        return false
-    }
-
-    fun jump(){
-        if(!isJumping && canJump) {
-            isJumping = true
-        }
-    }
-    fun gravity(l:List<Rectangle>){
-        if (rect.bottom < surface!!.height && !touchOne(l) && !isJumping){
-            y += 15
-            update()
-            if(touchOne(l)) {
-                y -= 15
-                canJump = true
-            }
-            else canJump = false
-        }
-        else{
-            canJump = true
-        }
-
-        update()
-    }
 }
